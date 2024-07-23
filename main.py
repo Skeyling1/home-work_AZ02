04b04e862f20dd6b8dc3cea1309e8cbde94e38a0
Skeyling1 <skeyling1@gmail.com>
2024-07-23 18:53:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,68 @@
 
 import pandas as pd
 import numpy as np
+import random
 
 pupils_list = {
-    "aaa" :
+    "ученики" : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Antonina', 'Serafin', 'Aleksandra', 'Robert', 'Paula'],
+    "математика" : [],
+    "физкультура" : [],
+    "литература": [],
+    "музыка": [],
+    "технология": [],
+
 }
 
-df = pd.DataFrame()
+for i in range(10):
+    mark = random.randint(1, 5)
+    pupils_list["математика"].append(mark)
+
+for i in range(10):
+    mark = random.randint(1, 5)
+    pupils_list["физкультура"].append(mark)
+
+for i in range(10):
+    mark = random.randint(1, 5)
+    pupils_list["литература"].append(mark)
+
+for i in range(10):
+    mark = random.randint(1, 5)
+    pupils_list["музыка"].append(mark)
+
+for i in range(10):
+    mark = random.randint(1, 5)
+    pupils_list["технология"].append(mark)
+
+df = pd.DataFrame(pupils_list)
+
+print(df, "\n")
+print('\tсредние оценки:')
+print('математика', df['математика'].mean())
+print('физкультура', df['физкультура'].mean())
+print('литература', df['литература'].mean())
+print('музыка', df['музыка'].mean())
+print('технология', df['технология'].mean())
+
+print('\n\tмедианные оценки:')
+print('математика', df['математика'].median())
+print('физкультура', df['физкультура'].median())
+print('литература', df['литература'].median())
+print('музыка', df['музыка'].median())
+print('технология', df['технология'].median())
+
+Q1_math = df['математика'].quantile(0.25)
+Q3_math = df['математика'].quantile(0.75)
+print('\nQ1 по математике:', Q1_math)
+print('Q3 по математике:', Q3_math)
+
+#Межквартальный размах (IQR) вычисляется как разница между третьим и первым квартилями.
+# Межквартальный размах показывает, как широко разбросаны средних 50 процентов значений, которые здесь есть
+print('IQR по математике:', Q3_math - Q1_math)
+
+print('\n\tстандартные отклонения:')
+print('математика', round(df['математика'].std(), 4))
+print('физкультура', round(df['физкультура'].std(), 4))
+print('литература', round(df['литература'].std(), 4))
+print('музыка', round(df['музыка'].std(), 4))
+print('технология', round(df['технология'].std(), 4))
+
